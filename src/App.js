a866f8f3c580ad9447b198d9fd2e341060301d5d
dsantos747 <d.santos74747@gmail.com>
2023-09-11 18:17:56 +0100

    Initial commit for project submission

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
+  function handleTextChange(event) {
+    setTextIn(event.target.value);
+  }
+
+  const defaultText =
+    "# Heading 1\n" +
+    "## Another heading\n" +
+    "[link](www.freecodecamp.org/learn)\n" +
+    "\n" +
+    "`inline code`\n" +
+    "```\n" +
+    "multi line code 1\n" +
+    "multi line code 2\n" +
+    "```\n" +
+    "> Block quotes\n" +
+    "* A\n" +
+    "* B\n" +
+    "* C\n" +
+    "\n" +
+    "![logo](https://svg-files.pixelied.com/1c656c89-67da-4e8b-8584-623127d4310b/thumb-256px.png)\n\n" +
+    "__mows__";
+  const [textIn, setTextIn] = useState(defaultText); // This is the setState of new react. data is the current data, and setData is the function to update the current state (data). data replaces this.data
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <textarea id="editor" value={textIn} onChange={handleTextChange} placeholder={defaultText} />
+      </div>
+
+      <div id="preview">
+        <ReactMarkdown id="preview">{textIn}</ReactMarkdown>
+      </div>
     </div>
   );
 }
